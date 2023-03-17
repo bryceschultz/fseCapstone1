93b013890329d0efc8b12bc0b00dc97832be7fb8
Bryce Schultz <bryceschultz26@gmail.com>
2023-03-17 16:57:39 -0400

    making dark mode work better

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ navControl.addEventListener("click", () => {
         navMenu.classList.add("hidden");
     }
 });
+
+var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    themeToggleLightIcon.classList.remove('hidden');
+} else {
+    themeToggleDarkIcon.classList.remove('hidden');
+}
